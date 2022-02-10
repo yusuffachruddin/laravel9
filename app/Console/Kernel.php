22e536926563e9fa0f1d65de3f93c836cf14ff56
Yusuf Fachruddin <yusuf.fachruddin@gmail.com>
2022-02-10 11:10:02 +0700

    first commit

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
     }
 
     /**
-     * Register the commands for the application.
+     * Register the commands for the application.f
      *
      * @return void
      */
